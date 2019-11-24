6348b586925492d323caca0cc72d4e6778f57d5a
Reese Grimsley <rgrimsle@andrew.cmu.edu>
2019-11-23 16:07:38 -0800

    Adding ISR and timer code

--- Software/SensorInterrupt_2/SensorInterrupt_2.ino
+++ Software/SensorInterrupt_2/SensorInterrupt_2.ino
@@ -22,9 +22,38 @@
 //clock and timer defines
 #define CPU_HZ 48000000 //CPU runs at 48MHz
 #define TIMER_TRIGGER_PRESCALE 1 // don't have to use this, but 32 is the max value before we sacrifice resolution
+#define FAST_SAMPLE_DELAY CPU_HZ/TIMER_TRIGGER_PRESCALE/10
+#define SLOW_SAMPLE_DELAY CPU_HZ/TIMER_TRIGGER_PRESCALE
+#define US_THRESHOLD CPU_HZ*50/1000 //~50ms
+
 
 int32_t duration, cm, inches, d_delta, d_new, d_old = 0;
 int32_t person_counter = 0;
+
+int32_t timer_value = 0;
+uint8_t fast_sample_flag = 0;
+
+void ultraSonicISR(void) {
+  Serial.println("ISR enter");
+  timer_value = TCC0->COUNT.reg;
+  Serial.print("Timer reads: "); Serial.println(timer_value);
+  if (fast_sample_flag){
+    if (timer_value > US_THRESHOLD)
+      fast_sample_flag = 0;
+      setTriggerTimerCC(SLOW_SAMPLE_DELAY);
+      person_counter++; //can make more complex; this will cause many duplicates
+  }
+
+  else {
+    if (timer_value < US_THRESHOLD) {
+      fast_sample_flag = 1;
+      setTriggerTimerCC(FAST_SAMPLE_DELAY);
+    }
+  }
+
+  
+  Serial.println("ISR end");
+}
 
 void blink(uint32_t ms) {
   digitalWrite(LED, HIGH);
@@ -76,10 +105,33 @@ float convertCM(uint32_t us_duration) {
   return (us_duration / 2) / 29.1; //convert to centimeters
 }
 
-void setupTriggerTimerCC() {
-  Serial.println("Set Timer values");
+void setupDistanceTimer() {
+  Serial.println("Configure PWM read TCC peripheral");
   
-  uint32_t compareValueWait = (uint32_t) CPU_HZ * 1 / TIMER_TRIGGER_PRESCALE; //set timer for 100ms
+  REG_PM_APBCMASK |= PM_APBCMASK_TCC0;
+  REG_GCLK_CLKCTRL |= (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TCC0_TCC1);
+  while ( GCLK->STATUS.bit.SYNCBUSY == 1 ); // wait for sync
+
+  TCC0->CTRLA.reg = TCC_CTRLA_SWRST;
+  while(TCC0->SYNCBUSY.bit.SWRST);
+
+  TCC0->CTRLA.reg |= TCC_CTRLA_CPTEN0 | TCC_CTRLA_RUNSTDBY;
+  TCC0->EVCTRL.reg |= TCC_EVCTRL_MCEI0 | TCC_EVCTRL_TCEI0 | TCC_EVCTRL_TCEI1 |
+            TCC_EVCTRL_EVACT0_RETRIGGER | TCC_EVCTRL_EVACT1_STOP;
+
+  while(TCC0->SYNCBUSY.reg);
+  TCC0->CTRLA.reg |= TCC_CTRLA_ENABLE;
+  while(TCC0->SYNCBUSY.reg);
+
+  Serial.println("Done configuring Timer");
+}
+
+
+void setTriggerTimerCC(uint32_t wait) {
+  Serial.println("Set Timer values");
+
+  uint32_t compareValueWait = wait;
+  //uint32_t compareValueWait = (uint32_t) CPU_HZ * 1 / TIMER_TRIGGER_PRESCALE; //set timer for 1s (old)
   uint32_t compareValueTrig = (uint32_t) CPU_HZ * 1E-5 / TIMER_TRIGGER_PRESCALE; //set a 10us pulse using a 48MHz base clock (APB)
 
   Serial.println(compareValueWait);
@@ -93,6 +145,7 @@ void setupTriggerTimerCC() {
   TC->CC[0].reg = compareValueWait; //set compare register; should actually be the TOP value
   TC->CC[1].reg = compareValueTrig; //set compare register
   while (TC->STATUS.bit.SYNCBUSY == 1);
+
 }
 
 void setTriggerTimer() {
@@ -110,73 +163,19 @@ void setTriggerTimer() {
   TC->CTRLA.reg &= ~TC_CTRLA_ENABLE; //disable timer so we can make changes
   while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
 
-   TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT32 | TC_CTRLA_WAVEGEN_MPWM; // | TC_CTRLA_RUNSTDBY;
+   TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT32 | TC_CTRLA_WAVEGEN_MPWM | TC_CTRLA_RUNSTDBY;
 //   TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT32 | TC_CTRLA_WAVEGEN_NPWM | TC_CTRLA_RUNSTDBY;
 
    while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
 
-   setupTriggerTimerCC();
+   setTriggerTimerCC(SLOW_SAMPLE_DELAY);
 
    TC->CTRLA.reg |= TC_CTRLA_ENABLE;
    while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
-}
 
-/* Sense: 
- * None, Rise, Fall, Both, High, Low
- * 0x0   0x1   0x2   0x3   0x4   0x5
- */
-void config_eic_channel(int ch, int sense, bool filt) {
-  // Config channel
-  EIC->CONFIG[ch/8].reg &= ~(0xF << 4*(ch%8));
-  EIC->CONFIG[ch/8].reg |= (0xF & ((filt? 0x8 : 0) | (0x7 & sense))) << 4*(ch%8);
-  // No wake-up
-  EIC->WAKEUP.reg &= ~(1 << ch);  
-  // No interrupt
-  EIC->INTENCLR.reg |= 1<<ch;
-  // Generate Event 
-  EIC->EVCTRL.reg |= 1<<ch;
-}
+   Serial.println("Did we get to the end of TriggerTimer");
 
-void config_eic() {
-  PM->APBAMASK.reg |= PM_APBAMASK_EIC;
-  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(EIC_GCLK_ID) | 
-              GCLK_CLKCTRL_CLKEN | 
-              GCLK_CLKCTRL_GEN(0);
-  EIC->CTRL.reg = EIC_CTRL_SWRST;
-  while(EIC->CTRL.bit.SWRST && EIC->STATUS.bit.SYNCBUSY);
-
-  // TODO: May need to change channel number (11)
-  // Set to 1 for Rising Action
-  config_eic_channel(11, 1, false);
-
-  // Do we need another for Falling?
-  //config_eic_channel(12, 2, false);   
- 
-  EIC->CTRL.bit.ENABLE = 1;
-  while(EIC->STATUS.bit.SYNCBUSY);
-}
-
-void config_evsys() {
-  PM->APBCMASK.reg |= PM_APBCMASK_EVSYS;
-  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID(EVSYS_GCLK_ID_0) |
-              GCLK_CLKCTRL_CLKEN |
-              GCLK_CLKCTRL_GEN(0);
-  while(GCLK->STATUS.bit.SYNCBUSY);
-
-  EVSYS->CTRL.bit.SWRST = 1;
-  while(EVSYS->CTRL.bit.SWRST);
-
-  // Event receiver
-  EVSYS->USER.reg = EVSYS_USER_CHANNEL(1) | // Set channel n-1
-              EVSYS_USER_USER(EVSYS_ID_USER_TCC1_EV_1); // Match/Capture 1 on TCC1
-  // Event channel
-  EVSYS->CHANNEL.reg = EVSYS_CHANNEL_CHANNEL(0) | // Set channel n
-           EVSYS_CHANNEL_PATH_ASYNCHRONOUS | 
-                 EVSYS_CHANNEL_EVGEN(EVSYS_ID_GEN_EIC_EXTINT_11) |
-                 EVSYS_CHANNEL_EDGSEL_BOTH_EDGES; // Detect both edges  
-  // Wait channel to be ready
-  while(!EVSYS->CHSTATUS.bit.USRRDY0);
-  // EVSYS is always enabled
+   //need to configure pin to be driven by this
 }
 
 void setup() {
@@ -195,27 +194,16 @@ void setup() {
 
   d_old = 0;
 
-  Serial.println(digitalPinHasPWM(8));
-  Serial.println(digitalPinHasPWM(12));
-
   Serial.println("Configure Timer");
   setTriggerTimer();
-
-  Serial.println("Configure EIC, Events, and GPIO");
-  Serial.println(EIC->CTRL.bit.ENABLE);
-  Serial.println(EVSYS->USER.reg);
-  
-  config_eic();
-  config_evsys();
-  Serial.println(EIC->CTRL.bit.ENABLE);
-  Serial.println(EVSYS->USER.reg);
+  setupDistanceTimer();
 }
 
 void loop() {
   #ifdef DEBUG_PRINT
   Serial.println("LOOP BEGIN");
 
-  //idle_state();
+  idle_state();
 
   TcCount32* TC = (TcCount32*) TC4;
   Serial.println(TC->COUNT.reg);
